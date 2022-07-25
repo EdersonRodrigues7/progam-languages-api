dc31ab08229aa2dbeccd8f5ac6f615e4858783a2
Ederson <ederson@trilhante.com.br>
2022-07-25 10:55:34 -0300

    chore<controllers>: adapted controller

--- src/main/java/br/com/rodriguestech/languagesapi/controllers/LanguagesController.java
+++ src/main/java/br/com/rodriguestech/languagesapi/controllers/LanguagesController.java
@@ -2,34 +2,46 @@ package br.com.rodriguestech.languagesapi.controllers;
 
 import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
-
-import br.com.rodriguestech.languagesapi.domain.entity.Language;
-import br.com.rodriguestech.languagesapi.repositories.LanguageRepository;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import jakarta.validation.Valid;
+import br.com.rodriguestech.languagesapi.domain.dto.CreateLanguageRequest;
+import br.com.rodriguestech.languagesapi.domain.dto.LanguageResponse;
+import br.com.rodriguestech.languagesapi.domain.dto.UpdateLanguageRequest;
+import br.com.rodriguestech.languagesapi.services.LanguagesService;
 
 @RestController
 public class LanguagesController {
+    private final LanguagesService service;
 
-    @Autowired
-    private LanguageRepository repository;
+    public LanguagesController(LanguagesService service) {
+        this.service = service;
+    }
 
-    @GetMapping(value="/languages")
-
-    public List<Language> getLanguages(){
-        List<Language> languages = repository.findAll();
-        return languages;
+    public ResponseEntity<List<LanguageResponse>>  getAll() {
+        var result = service.listAll();
+        return ResponseEntity.ok(result);
     }
 
     @PostMapping(value="/languages")
     @ResponseStatus(HttpStatus.CREATED)
-    public Language addLanguage(@RequestBody Language language){
-        var newLanguage = repository.save(language);
-        return newLanguage;
+    public ResponseEntity<LanguageResponse> create(@RequestBody @Valid CreateLanguageRequest request) {
+        var result = service.createLanguage(request);
+        return ResponseEntity.ok(result);
+    }
+
+    @PatchMapping("/languages/{id}")
+    public ResponseEntity<LanguageResponse> update(
+            @PathVariable("id") String id,
+            @RequestBody @Valid UpdateLanguageRequest updateRequest) {
+        var language = service.updateLanguage(id, updateRequest);
+        return ResponseEntity.ok(language);
+    }
+
+    @DeleteMapping("/languages/{id}")
+    public ResponseEntity<LanguageResponse> delete(@PathVariable("id") String id) {
+        var language = service.delete(id);
+        return ResponseEntity.ok(language);
     }
 }
