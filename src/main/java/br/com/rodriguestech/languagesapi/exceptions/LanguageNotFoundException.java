25bf58b4e60923f80b467a966d8bbe36019fc02c
Ederson <ederson@trilhante.com.br>
2022-07-25 10:46:14 -0300

    chore<exceptions>: language not found

---
+++ src/main/java/br/com/rodriguestech/languagesapi/exceptions/LanguageNotFoundException.java
@@ -0,0 +1,11 @@
+package br.com.rodriguestech.languagesapi.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(code = HttpStatus.NOT_FOUND)
+public class LanguageNotFoundException extends RuntimeException {
+    public LanguageNotFoundException() {
+        super("Language not found.");
+    }
+}
